d89bc368216f5925c38aa20325993a1bee34a507
jarod <gregory@jarod-xp.com>
2020-01-17 14:19:59 +0100

    Toggle front sidebar js added

--- public/assets/js/sidebar-toggle.js
+++ public/assets/js/sidebar-toggle.js
@@ -3,3 +3,9 @@ $("#adminToggleBtn").click(function(e) {
     $("#sidebar-nav").toggleClass("hidden");
     $("#page-content-wrapper").toggleClass("col-md-10 offset-md-2 hidden");
 });
+
+$("#frontToggleBtn").click(function(e) {
+    e.preventDefault();
+    $("#front-sidebar").toggleClass("hidden");
+    $("#frontToggleBtn").toggleClass("hidden");
+});
