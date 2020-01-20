9be5c65c1b5a1ec9f75577bed879b6cb311bb98b
jarod <gregory@jarod-xp.com>
2020-01-20 11:43:11 +0100

    SQL creation file updated

--- blog.sql
+++ blog.sql
@@ -1,164 +1,116 @@
--- phpMyAdmin SQL Dump
--- version 4.9.2
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3308
--- Généré le :  sam. 18 jan. 2020 à 16:33
--- Version du serveur :  5.7.28
--- Version de PHP :  7.4.0
+create table comments
+(
+    id         int auto_increment
+        primary key,
+    post_id    int               not null,
+    pseudo     varchar(300)      null,
+    content    mediumtext        not null,
+    date_added datetime          not null,
+    status     tinyint default 0 not null
+)
+    engine = InnoDB;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+create index post_id
+    on comments (post_id);
+
+create table network_links
+(
+    id         int auto_increment
+        primary key,
+    network_id int          not null,
+    user_id    int          not null,
+    link       varchar(300) not null
+)
+    engine = InnoDB;
+
+create index network_id
+    on network_links (network_id);
+
+create index user_id
+    on network_links (user_id);
+
+create table social_networks
+(
+    id        int auto_increment
+        primary key,
+    name      varchar(300) null,
+    upload_id int          not null,
+    constraint name
+        unique (name),
+    constraint upload_id
+        unique (upload_id)
+)
+    engine = InnoDB;
+
+create table uploads
+(
+    id            int auto_increment
+        primary key,
+    file_name     varchar(300) default 'NULL' not null,
+    original_name varchar(300) default 'NULL' not null,
+    alt           varchar(300)                null,
+    constraint file_name
+        unique (file_name),
+    constraint original_name
+        unique (original_name)
+)
+    engine = InnoDB;
+
+create table posts
+(
+    id            int auto_increment
+        primary key,
+    user_id       int               not null,
+    title         varchar(300)      null,
+    header_id     int               null,
+    extract       mediumtext        null,
+    content       mediumtext        null,
+    date_added    datetime          not null,
+    date_modified datetime          not null,
+    status        tinyint default 0 null,
+    constraint posts_ibfk_1
+        foreign key (header_id) references uploads (id)
+)
+    engine = InnoDB;
+
+create index header_id
+    on posts (header_id);
+
+create index user_id
+    on posts (user_id);
+
+create table users
+(
+    id           int auto_increment
+        primary key,
+    username     varchar(300)                null,
+    email        varchar(300) default 'NULL' not null,
+    password     varchar(300)                not null,
+    role         tinyint                     not null,
+    avatar_id    int                         null,
+    first_name   varchar(300)                null,
+    last_name    varchar(500)                null,
+    title        varchar(300)                null,
+    phone        varchar(300)                null,
+    baseline     mediumtext                  null,
+    introduction mediumtext                  null,
+    resume_id    int                         null,
+    date_added   datetime                    not null,
+    constraint email
+        unique (email),
+    constraint username
+        unique (username),
+    constraint users_ibfk_1
+        foreign key (avatar_id) references uploads (id),
+    constraint users_ibfk_2
+        foreign key (resume_id) references uploads (id)
+)
+    engine = InnoDB;
+
+create index avatar_id
+    on users (avatar_id);
+
+create index resume_id
+    on users (resume_id);
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données :  `blog`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `comments`
---
-
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE IF NOT EXISTS `comments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `post_id` int(11) NOT NULL,
-  `pseudo` varchar(300) DEFAULT NULL,
-  `content` mediumtext NOT NULL,
-  `date_added` datetime NOT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `post_id` (`post_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `network_links`
---
-
-DROP TABLE IF EXISTS `network_links`;
-CREATE TABLE IF NOT EXISTS `network_links` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `network_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `link` varchar(300) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `network_id` (`network_id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `posts`
---
-
-DROP TABLE IF EXISTS `posts`;
-CREATE TABLE IF NOT EXISTS `posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `title` varchar(300) DEFAULT NULL,
-  `header_id` int(11) DEFAULT NULL,
-  `extract` mediumtext,
-  `content` mediumtext,
-  `date_added` datetime NOT NULL,
-  `date_modified` datetime NOT NULL,
-  `status` tinyint(4) DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `header_id` (`header_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `social_networks`
---
-
-DROP TABLE IF EXISTS `social_networks`;
-CREATE TABLE IF NOT EXISTS `social_networks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(300) DEFAULT NULL,
-  `upload_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `upload_id` (`upload_id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `uploads`
---
-
-DROP TABLE IF EXISTS `uploads`;
-CREATE TABLE IF NOT EXISTS `uploads` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `file_name` varchar(300) NOT NULL DEFAULT 'NULL',
-  `original_name` varchar(300) NOT NULL DEFAULT 'NULL',
-  `alt` varchar(300) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `file_name` (`file_name`),
-  UNIQUE KEY `original_name` (`original_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(300) DEFAULT NULL,
-  `email` varchar(300) NOT NULL DEFAULT 'NULL',
-  `password` varchar(300) NOT NULL,
-  `role` tinyint(4) NOT NULL,
-  `avatar_id` int(11) DEFAULT NULL,
-  `first_name` varchar(300) DEFAULT NULL,
-  `last_name` varchar(500) DEFAULT NULL,
-  `title` varchar(300) DEFAULT NULL,
-  `phone` varchar(300) DEFAULT NULL,
-  `baseline` mediumtext,
-  `introduction` mediumtext,
-  `resume_id` int(11) DEFAULT NULL,
-  `date_added` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `username` (`username`),
-  KEY `avatar_id` (`avatar_id`),
-  KEY `resume_id` (`resume_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `posts`
---
-ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`header_id`) REFERENCES `uploads` (`id`);
-
---
--- Contraintes pour la table `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`avatar_id`) REFERENCES `uploads` (`id`),
-  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`resume_id`) REFERENCES `uploads` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
